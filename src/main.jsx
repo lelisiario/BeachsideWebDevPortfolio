7510972e79680adbf19d71b8d50ad431bd55b3ff
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-07-06 18:08:39 -0700

    Changed Structure to meet technical requirements.

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import Error from './pages/Error';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
@@ -27,12 +28,16 @@ const router = createBrowserRouter([
         element: <About />,
       },
       {
-        path: '/Blog',
-        element: <Blog />,
+        path: '/Portfolio',
+        element: <Portfolio />,
       },
       {
         path: '/Contact',
         element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
       },
     ],
   },
